3755c565b71919d7f62feb84380d920a5b5d5c83
jaydgm <rjayd22@gmail.com>
2024-03-29 12:13:17 -0700

    Add routing a needed import statements

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,52 +1,16 @@
-import { useState } from 'react'
+import React from 'react';
+import {Route, Switch} from 'react-router-dom'
+import Login from './components/Login.jsx';
 import './App.css'
 
 function App() {
-  const [firstName,setFirstName] = useState('')
-  const [lastName,setLastName] = useState('')
-  
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-
-    try {
-      const response = await fetch('http://localhost:3000/users', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          firstName,
-          lastName,
-        }),
-      })
-
-      const data = await response.json()
-      console.log(data)
-
-      // clear input fields after submission
-      setFirstName('')
-      setLastName('')
-    } catch (error) {
-      console.error('error adding user:', error)
-    }
-  }
-
   return (
-    <div className="App">
-      <h1>Add User</h1>
-      <form onSubmit={handleSubmit}>
-        <label>
-          First Name:
-          <input type="text" value={firstName} onChange={(e) => setFirstName(e.target.value)} />
-        </label>
-        <label>
-          Last Name:
-          <input type="text" value={lastName} onChange={(e) => setLastName(e.target.value)} />
-        </label>
-        <button type="submit">Add User</button>
-      </form>
+    <div Classname="App">
+      <Switch>
+        <Route path="/login" component={Login} />
+      </Switch>
     </div>
   )
 }
 
-export default App
+export default App;
