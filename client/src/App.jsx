11db229382fd98031fb1131e81d998fee7241bd9
jaydgm <rjayd22@gmail.com>
2024-03-29 12:38:37 -0700

    App.jsx will be used as outlet for child routes of App in main.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,11 @@
-import React from 'react';
-import {Route, Switch} from 'react-router-dom'
-import Login from './components/Login.jsx';
-import './App.css'
+import { Outlet } from "react-router-dom";
 
 function App() {
   return (
-    <div Classname="App">
-      <Switch>
-        <Route path="/login" component={Login} />
-      </Switch>
+    <div className="App">
+      <Outlet />
     </div>
-  )
+  );
 }
 
 export default App;
