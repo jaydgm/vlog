9728f70dffdae4269bd54d10756e0ef613a33065
jaydgm <rjayd22@gmail.com>
2024-04-02 12:59:12 -0700

    Dependencies for tailwind

---
+++ client/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+
+export default {
+  content: [
+    "./src/components/Signup.jsx",
+    "./src/components/Login.jsx",
+  ],
+  darkMode: 'class',
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
