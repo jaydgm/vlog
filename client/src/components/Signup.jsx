35274a0ccb6df2242cc44c9ed88ef8e1166f9f2a
jaydgm <rjayd22@gmail.com>
2024-04-03 15:45:37 -0700

    Add onSubmit to form tag

--- client/src/components/Signup.jsx
+++ client/src/components/Signup.jsx
@@ -57,7 +57,10 @@ function Signup() {
 
                       <p className="text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-4">Sign up</p>
 
-                      <form className="mx-1 mx-md-4">
+                      <form 
+                        className="mx-1 mx-md-4"
+                        onSubmit={handleSubmit}
+                        >
 
                         <div className="d-flex flex-row align-items-center mb-4">
                           <FontAwesomeIcon icon={faUser} className="fa-lg me-3 fa-fw" />
@@ -90,9 +93,9 @@ function Signup() {
                             <label className="form-label" htmlFor="form3Example4cd">Repeat your password</label>
                           </div>
                         </div>
-                        {passwordMatchError ? (
+                        {passwordMatchError && (
                           <div className="text-danger">Passwords do not match!</div>
-                          ) : setPasswordMatchError(false)}
+                        )}
                         <div className="form-check d-flex justify-content-center mb-5">
                           <input className="form-check-input me-2" type="checkbox" value="" id="form2Example3c" />
                           <label className="form-check-label" htmlFor="form2Example3">
@@ -101,7 +104,10 @@ function Signup() {
                         </div>
 
                         <div className="d-flex justify-content-center mx-4 mb-3 mb-lg-4">
-                          <button type="button" className="btn btn-primary btn-lg">Register</button>
+                          <button  
+                            type="submit" 
+                            className="btn btn-primary btn-lg"
+                            >Register</button>
                         </div>
                       </form>
                     </div>
