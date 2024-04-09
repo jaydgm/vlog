56aa5b9db090fcb77d11eaf9840e712e43d54e5d
jaydgm <rjayd22@gmail.com>
2024-04-09 11:49:21 -0700

    Create jwt.js file to handle jwt in local storage

---
+++ client/src/auth/jwt.js
@@ -0,0 +1,21 @@
+
+// sets and saves jwt of user to 
+// local storage
+export const setJwt = (jwt) => {
+
+    if (!jwt) {
+        return "Missing jwt token";
+    }
+
+    localStorage.setItem("jwt", `Bearer ${jwt}`)
+}
+
+// clears local storage
+export const clearJwt = () => {
+    localStorage.clear();
+}
+
+// gets jwt in local storage
+export const getJwt = () => {
+    return localStorage.getItem("jwt");
+}
