dcfc7f926a199db2bc85e390f3b1804d8dc48137
jaydgm <rjayd22@gmail.com>
2024-04-12 15:23:40 -0700

    Add basic UI for homepage
    added button to schedule meeting, and started function to handle when user clicks button

--- client/src/components/Dashboard.jsx
+++ client/src/components/Dashboard.jsx
@@ -1,8 +1,29 @@
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUser, faEnvelope, faLock, faKey } from '@fortawesome/free-solid-svg-icons';
+import { getJwt } from "../auth/jwt";
+
 function Dashboard() {
+
+    handleScheduler = async () => {
+        const jwt = getJwt();
+    }
+
     return (
         <>
-            <h1>Home Page</h1>
+            {/* navbar */}
+            <nav className="navbar navbar-light bg-dark d-flex justify-content-center">
+                <span className="navbar-brand mb-0 h1 text-light">VLog</span>
+            </nav>
+            {/* hero */}
+            <div className="d-flex justify-content-center">
+                <button 
+                    type="button" 
+                    onClick={handleScheduler}
+                    className="btn btn-danger btn-lg">Schedule a meeting
+                </button>
+            </div>
         </>
     )
 }
