ca62fc35dc5b7e8bff2ed28222c644fb06c41cca
jaydgm <rjayd22@gmail.com>
2024-04-22 11:12:05 -0700

    Add dropdown menu

--- client/src/components/Scheduler.jsx
+++ client/src/components/Scheduler.jsx
@@ -1,17 +1,27 @@
-
 import 'bootstrap/dist/css/bootstrap.min.css';
+import '@popperjs/core';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faUser, faEnvelope, faLock, faKey } from '@fortawesome/free-solid-svg-icons';
 import { getJwt } from "../auth/jwt";
 import { useNavigate } from 'react-router';
 
-function Dashboard() {
+function Scheduler() {
 
     return (
         <>
-            <h1>Schedule a meeting here</h1>
+            <h1>Choose a household</h1>
+            <div className="dropdown">
+                <button className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+                            Dropdown button
+                        </button>
+                <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+                    <li><a className="dropdown-item" href="#">Action</a></li>
+                    <li><a className="dropdown-item" href="#">Another action</a></li>
+                    <li><a className="dropdown-item" href="#">Something else here</a></li>
+                </ul>
+            </div>
         </>
     )
 }
 
-export default Dashboard;
+export default Scheduler;
