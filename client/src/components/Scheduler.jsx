a355fad72674ac491dd123fe54883d5a07de7ef5
jaydgm <rjayd22@gmail.com>
2024-04-22 11:29:29 -0700

    Add functionality to replace placeholder with selected item

--- client/src/components/Scheduler.jsx
+++ client/src/components/Scheduler.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '@popperjs/core';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
@@ -7,17 +8,18 @@ import { useNavigate } from 'react-router';
 
 function Scheduler() {
 
+    const [selectedItem, setSelectedItem] = useState('');
+
     return (
         <>
             <h1>Choose a household</h1>
             <div className="dropdown">
-                <button className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
-                            Dropdown button
-                        </button>
+                <button className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false"> 
+                    {selectedItem ? selectedItem : "Dropdown Link"}
+                </button>
                 <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton1">
-                    <li><a className="dropdown-item" href="#">Action</a></li>
-                    <li><a className="dropdown-item" href="#">Another action</a></li>
-                    <li><a className="dropdown-item" href="#">Something else here</a></li>
+                <li><a className="dropdown-item" href="#" onClick={() => setSelectedItem('Action')}>Action</a></li>
+
                 </ul>
             </div>
         </>
