ec3304a92929343f5e96fe02ec64da220ea8a93d
jaydgm <rjayd22@gmail.com>
2024-04-24 12:33:46 -0700

    Allow users to click Vlog in navbar and direct back to dashboard

--- client/src/Layout.jsx
+++ client/src/Layout.jsx
@@ -1,11 +1,11 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, Link } from "react-router-dom";
 
 function Layout() {
   return (
     <div className="layout">
       {/* navbar */}
       <nav className="navbar navbar-light bg-dark d-flex justify-content-center">
-          <span className="navbar-brand mb-0 h1 text-light">VLog</span>
+          <Link to='/vlog' className="navbar-brand mb-0 h1 text-light">VLog</Link>
       </nav>
       <Outlet />
     </div>
