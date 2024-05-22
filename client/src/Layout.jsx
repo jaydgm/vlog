dbcecd33d18d29def42d5090d8f7aff4c8cbb36f
jaydgm <rjayd22@gmail.com>
2024-05-22 15:13:59 -0700

    Resize navbar

--- client/src/Layout.jsx
+++ client/src/Layout.jsx
@@ -4,8 +4,8 @@ function Layout() {
   return (
     <div className="layout">
       {/* navbar */}
-      <nav className="navbar navbar-light bg-dark d-flex justify-content-center">
-          <Link to='/vlog' className="navbar-brand mb-0 h1 text-light">VLog</Link>
+      <nav className="navbar navbar-light bg-dark py-4 d-flex justify-content-center">
+          <Link to='/vlog' className="navbar-brand mb-0 h1 text-light fs-1">VLog</Link>
       </nav>
       <Outlet />
     </div>
