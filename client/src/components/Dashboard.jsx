b3adaf3ce954806eb0c4fbe1c69ed1306add7f0d
jaydgm <rjayd22@gmail.com>
2024-05-30 15:53:54 -0700

    Hook up endpoint to get visitations

--- client/src/components/Dashboard.jsx
+++ client/src/components/Dashboard.jsx
@@ -4,8 +4,15 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faUser, faEnvelope, faLock, faKey } from '@fortawesome/free-solid-svg-icons';
 import { getJwt } from "../auth/jwt";
 import { useNavigate } from 'react-router';
+import { useEffect, useState } from 'react';
 
 function Dashboard() {
+
+    const [visitations, setVisitations] = useState([])
+
+    useEffect(() => {
+        getVisitations()
+    , []})
 
     const navigate = useNavigate()
     const jwt = getJwt();
@@ -22,7 +29,6 @@ function Dashboard() {
 
         const data = await response.json()
 
-        
         if (data.err === "Invalid authorization, no authorization headers" || 
         data.err === "Invalid authorization, invalid authorization scheme") {
             console.log('Error authenticating jwt')
@@ -35,7 +41,28 @@ function Dashboard() {
         }
     }
 
-    
+    const getVisitations = async () => {
+        try {
+            const response = await fetch('http://localhost:3000/show-visitations', {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+            'authorization': jwt,
+          },
+        })
+
+        const { success, data } = await response.json()
+
+        if (success) {
+            setVisitations(data)
+        } else {
+            window.alert('error getting visitations')
+        }
+        
+        } catch (error) {
+            console.log(error)
+        }
+    }   
 
     return (
         <>
@@ -55,6 +82,8 @@ function Dashboard() {
                     <th scope="col">Member</th>
                     <th scope="col">Attendees</th>
                     <th scope="col">Date</th>
+                    <th scope="col">Time</th>
+
                     </tr>
                 </thead>
                 <tbody>
@@ -63,17 +92,26 @@ function Dashboard() {
                     <td>Mark</td>
                     <td>Otto</td>
                     <td>@mdo</td>
+                    <td></td>
                     </tr>
                     <tr>
                     <th scope="row">2</th>
                     <td>Jacob</td>
                     <td>Thornton</td>
                     <td>@fat</td>
+                    <td></td>
                     </tr>
                     <tr>
                     <th scope="row">3</th>
                     <td colspan="2">Larry the Bird</td>
                     <td>@twitter</td>
+                    <td></td>
+                    </tr>                    
+                    <tr>
+                    <th scope="row">4</th>
+                    <td colspan="2">Larry the Bird</td>
+                    <td>@twitter</td>
+                    <td></td>
                     </tr>
                 </tbody>
             </table>
