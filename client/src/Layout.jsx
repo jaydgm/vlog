e2661c5625da42c60b6c71de2c492a65637461b0
jaydgm <rjayd22@gmail.com>
2024-06-03 11:31:06 -0700

    Add footer and logout button

--- client/src/Layout.jsx
+++ client/src/Layout.jsx
@@ -4,11 +4,15 @@ function Layout() {
   return (
     <div className="layout">
       {/* navbar */}
-      <nav className="navbar navbar-light bg-dark py-4 d-flex justify-content-center">
+      <nav className="navbar navbar-light bg-dark py-3 d-flex justify-content-center">
           <Link to='/vlog' className="navbar-brand mb-0 h1 text-light fs-1">VLog</Link>
+      </nav>
+      <nav className="footer d-flex justify-content-center align-items-center fixed-bottom navbar-dark bg-dark">
+                <Link to='/login' className="navbar-brand mb-0 h1 text-light fs-1">Logout</Link>
       </nav>
       <Outlet />
     </div>
+    
   );
 }
 
