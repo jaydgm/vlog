f466ae2f7e87acffde1fa6477509d7a5e8dc61be
jaydgm <rjayd22@gmail.com>
2024-06-04 13:00:17 -0700

    Remove previous way to schedule

--- client/src/components/Dashboard.jsx
+++ client/src/components/Dashboard.jsx
@@ -18,30 +18,6 @@ function Dashboard() {
     const navigate = useNavigate()
     const jwt = getJwt();
 
-    const handleScheduler = async () => {
-        try {
-            const response = await fetch('http://localhost:3000/scheduler', {
-          method: 'GET',
-          headers: {
-            'Content-Type': 'application/json',
-            'authorization': jwt,
-          },
-        })
-
-        const data = await response.json()
-
-        if (data.err === "Invalid authorization, no authorization headers" || 
-        data.err === "Invalid authorization, invalid authorization scheme") {
-            console.log('Error authenticating jwt')
-        } else if (data.success) {
-            navigate('/vlog/new')
-        }
-        
-        } catch (error) {
-            console.log(error)
-        }
-    }
-
     const handleDelete = async (visitation_id) => {
         try {
             const response = await fetch(`http://localhost:3000/delete-visitation?visitation_id=${visitation_id}`, {
@@ -106,16 +82,8 @@ function Dashboard() {
 
     return (
         <>
-            {/* hero */}
-            <div className="heroContainer d-flex justify-content-center align-items-center">
-                <button 
-                    className="scheduleBtn rounded-pill" 
-                    type="button"
-                    onClick={handleScheduler}>
-                    Schedule Visitation</button>
-            </div>
             {/* table for scheduled meetings */}
-            <table class="table table-success table-striped">
+            <table className="table table-success table-striped">
                 <thead>
                     <tr>
                     <th scope="col">#</th>
