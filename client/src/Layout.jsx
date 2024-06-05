d87093ca8949e4f62bff8a06c17f5d3db7247786
jaydgm <rjayd22@gmail.com>
2024-06-04 19:50:51 -0700

    Remove modal logic and change to pass to ShcedulerModal file

--- client/src/Layout.jsx
+++ client/src/Layout.jsx
@@ -5,21 +5,15 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCirclePlus } from '@fortawesome/free-solid-svg-icons';
 import { useNavigate } from 'react-router';
 import { getJwt } from "./auth/jwt";
+import SchedulerModal from './components/SchedulerModal'
 
 function Layout() {
-  const [showModal, setShowModal] = useState(false); // State to manage modal visibility
+  const [showModal, setShowModal] = useState(false)
+
   const navigate = useNavigate();
   const jwt = getJwt();
 
-  // Function to open the modal
-  const openModal = () => {
-    setShowModal(true);
-  }
 
-  // Function to close the modal
-  const closeModal = () => {
-    setShowModal(false);
-  }
 
   // Function to handle scheduler
   const handleScheduler = async () => {
@@ -45,6 +39,11 @@ function Layout() {
     }
   }
 
+  const handleModal = () => {
+    setShowModal(prevShowModal => !showModal)
+    console.log(showModal)
+  }
+
   return (
     <div className="layout" style={{backgroundColor: '#343a40'}}>
       {/* Navbar */}
@@ -53,31 +52,10 @@ function Layout() {
           <img src={Vlog} alt="Vlog" style={{ width: '100px', height: '100px' }} />
         </Link>
         {/* Add Icon */}
-        <button type='button' onClick={openModal} style={{ background: 'transparent', border: 'none', cursor: 'pointer' }}>
+        <button type='button' onClick={handleModal} style={{ background: 'transparent', border: 'none', cursor: 'pointer' }}>
           <FontAwesomeIcon icon={faCirclePlus} size="lg" style={{ color: "#8CB561", fontSize: "50px" }} />
         </button>
       </nav>
-
-      {/* Bootstrap Modal */}
-      {showModal && (
-        <div className="modal fade show" id="exampleModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true" style={{ display: 'block' }}>
-          <div className="modal-dialog">
-            <div className="modal-content">
-              <div className="modal-header">
-                <h5 className="modal-title" id="exampleModalLabel">Add Item</h5>
-                <button type="button" className="btn-close" onClick={closeModal} aria-label="Close"></button>
-              </div>
-              <div className="modal-body">
-                {/* Add your form or content for adding item here */}
-              </div>
-              <div className="modal-footer">
-                <button type="button" className="btn btn-secondary" onClick={closeModal}>Close</button>
-                {/* Add button to save item here */}
-              </div>
-            </div>
-          </div>
-        </div>
-      )}
 
       {/* Footer */}
       <nav className="footer d-flex justify-content-center align-items-center fixed-bottom navbar-dark bg-dark">
