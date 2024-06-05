78f46cfb538ae3936b9ff81c2513843720c91ae3
jaydgm <rjayd22@gmail.com>
2024-06-04 20:05:04 -0700

    pass showModal

--- client/src/components/SchedulerModal.jsx
+++ client/src/components/SchedulerModal.jsx
@@ -1,6 +1,21 @@
-import React from 'react'
+import { useState, useEffect } from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '@popperjs/core';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUser, faEnvelope, faLock, faKey } from '@fortawesome/free-solid-svg-icons';
+import { getJwt } from "../auth/jwt";
+import { Form } from 'react-bootstrap';
+import { useNavigate } from 'react-router';
 
-const SchedulerModal=({ handleModal }) => {
+const SchedulerModal=({ handleModal, showModal }) => {
+    const [members, setMembers] = useState([]);
+    const [selectedMember, setSelectedMember] = useState('');
+    const [selectedDate, setSelectedDate] = useState('');
+    const [selectedTime, setSelectedTime] = useState('');
+    const [users, setUsers] = useState([]);
+    const [selectedAttendee, setSelectedAttendee] = useState('');
+    const [attendees, setAttendees] = useState([])
+    const jwt = getJwt();
 
     return (
         <>
@@ -10,7 +25,7 @@ const SchedulerModal=({ handleModal }) => {
           <div className="modal-dialog">
             <div className="modal-content">
               <div className="modal-header">
-                <h5 className="modal-title" id="exampleModalLabel">Add Item</h5>
+                <h5 className="modal-title" id="exampleModalLabel">Add Itemssss</h5>
                 <button type="button" className="btn-close" onClick={handleModal} aria-label="Close"></button>
               </div>
               <div className="modal-body">
@@ -28,3 +43,5 @@ const SchedulerModal=({ handleModal }) => {
         </>
     )
 }
+
+export default SchedulerModal;
