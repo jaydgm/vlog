3a1906e626ab6af6a3c8d3679f607404a1346f3f
jaydgm <rjayd22@gmail.com>
2024-06-06 12:56:28 -0700

    Create file for password modal

---
+++ client/src/components/ChangePasswordModal.jsx
@@ -0,0 +1,20 @@
+import { useState, useEffect } from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '@popperjs/core';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUser, faEnvelope, faLock, faKey } from '@fortawesome/free-solid-svg-icons';
+import { getJwt } from "../auth/jwt";
+import { Form, Button } from 'react-bootstrap';
+import { useNavigate } from 'react-router';
+
+const ChangePasswordModal = ({ handlePasswordModal, showPasswordModal }) => {
+
+
+    return (
+        <>
+            
+        </>
+    );
+}
+
+export default ChangePasswordModal;
